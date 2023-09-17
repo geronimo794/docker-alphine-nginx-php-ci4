9f694d9c3925c2575c403035cb0a8dd425e8d9a9
Ach Rozikin@MacBook <geronimo794@gmail.com>
2023-09-17 23:22:56 +0700

    first commit

---
+++ build-push-image.sh
@@ -0,0 +1,39 @@
+#/bin/bash
+
+# Show all command and variable value
+set -x
+
+# Load configuration from .env file
+set -o allexport
+
+# If .env not exist then use format.env
+if [ -f build-push-image.env ]; then
+	source build-push-image.env
+else
+	echo "Please populate the build-push-image.env file from build-push-image.env.format"
+	exit
+fi
+set +o allexport
+
+# Hide all command and variable value again
+set +x
+
+# Build image from Docker file with var $IMAGE_REPO_NAME and tag $IMAGE_TAG
+# You can see it from .env configuration
+docker build --pull --rm -f "$DOCKER_FILE" -t $IMAGE_REPO_NAME:$IMAGE_TAG "."
+
+# Show all list of docker iamge
+docker image ls
+
+# Define full new image name, with registry, namespace, repo
+FULL_NEW_IMAGE_NAME=$GITHUB_REGISTRY/$GITHUB_USERNAME/$IMAGE_REPO_NAME:$IMAGE_TAG
+
+# Rename previous image repo with new repo and tag
+docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $FULL_NEW_IMAGE_NAME
+
+# Login with saved Personal Access Token and Github Username
+# You can configure it via .env file
+echo $GITHUB_PAT | docker login ghcr.io --username $GITHUB_USERNAME --password-stdin
+
+# Push the image to github registry
+docker push $FULL_NEW_IMAGE_NAME
